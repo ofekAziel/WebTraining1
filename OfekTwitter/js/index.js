d415a6db4e100b2354fdc5dcb94c00d0c9135655
ofekAziel <ofek200@gmail.com>
2016-12-11 16:34:24 +0200

    - project with testing

--- OfekTwitter/js/index.js
+++ OfekTwitter/js/index.js
@@ -24,7 +24,7 @@ window.onload = function () {
         .catch(function (error) {
             console.log(error);
 
-    });
+        });
 };
 
 var init = function () {
@@ -37,7 +37,7 @@ var init = function () {
 
 var createTweets = function () {
 
-    lastTweets.forEach(function(tweet) {
+    lastTweets.forEach(function (tweet) {
 
         if (userFollowingById.includes(tweet.user) || tweet.user === userId) {
 
@@ -78,5 +78,69 @@ var newTweet = function () {
     postNewTweet(userId, userTweet.value);
     userTweet.value = "";
 };
+
+//Testing
+
+describe("createTweets", function () {
+
+    userId = "10c06b27-d8ee-4435-9cee-0a2a838ca14a";
+    userName = "Marta";
+
+    it("tweets was not created user not follows them", function () {
+
+        lastTweets = [
+            {
+                "text": "Hello",
+                "user": "ff2b41b9-e1d8-4594-9aa3-c1dda30b0d22"
+            },
+            {
+                "text": "Bye",
+                "user": "ff2b41b9-e1d8-4594-9aa3-c1dda30b0d22"
+            }
+        ];
+
+        spyOn(window, 'createTweet');
+        createTweets();
+        expect(window.createTweet).not.toHaveBeenCalled();
+    });
+
+    it("tweets was created user follows them", function () {
+
+        lastTweets = [
+            {
+                "text": "Hello",
+                "user": "cc707c95-f1e3-4caf-906d-f9dd1f394b99"
+            },
+            {
+                "text": "Bye",
+                "user": "10c06b27-d8ee-4435-9cee-0a2a838ca14a"
+            }
+        ];
+
+        spyOn(window, 'createTweet');
+        createTweets();
+        expect(window.createTweet).toHaveBeenCalled();
+    });
+
+    it("tweets was not created last tweets array is empty", function () {
+
+        lastTweets = [];
+        spyOn(window, 'createTweet');
+        createTweets();
+        expect(window.createTweet).not.toHaveBeenCalled();
+    });
+});
+
+describe("createTweet", function () {
+
+    // it("tweet html was created", function () {
+    //
+    //     var allTweets = $("#last-tweets").elements[0];
+    //     var tweet = document.createElement("div");
+    //     spyOn(document, 'appendChild');
+    //     createTweet("Marta", "Hello", "green");
+    //     expect(allTweets.document.appendChild(tweet)).toHaveBeenCalled();
+    // });
+});
 
 
